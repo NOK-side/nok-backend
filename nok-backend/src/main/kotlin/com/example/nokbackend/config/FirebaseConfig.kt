d06839adcb5eab1bcea8c1a6d50da639062b7cd7
rkdals213 <rkdals213@naver.com>
2022-08-20 19:43:52 +0900

    feat: test

--- nok-backend/src/main/kotlin/com/example/nokbackend/config/FirebaseConfig.kt
+++ nok-backend/src/main/kotlin/com/example/nokbackend/config/FirebaseConfig.kt
@@ -1,8 +1,10 @@
 package com.example.nokbackend.config
 
+import com.example.nokbackend.util.createRandomString
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
@@ -15,16 +17,24 @@ class FirebaseConfig {
     @Value("\${app.firebase-configuration-file}")
     private val firebaseConfigPath: String = ""
 
-//    @PostConstruct
+    @PostConstruct
     fun initialize() {
+        val classPathResource = ClassPathResource(firebaseConfigPath)
+        log.error(classPathResource.uri.toString())
+        log.error(classPathResource.path)
+
         val options = FirebaseOptions.builder().setCredentials(
             GoogleCredentials.fromStream(
                 ClassPathResource(firebaseConfigPath).inputStream
             )
         ).build()
 
-        FirebaseApp.initializeApp(options)
+        FirebaseApp.initializeApp(options, createRandomString(10))
 
         check(FirebaseApp.getApps().isNotEmpty()) { "파이어베이스 초기화 실패" }
+    }
+
+    companion object {
+        val log = LoggerFactory.getLogger(this::class.java)
     }
 }
