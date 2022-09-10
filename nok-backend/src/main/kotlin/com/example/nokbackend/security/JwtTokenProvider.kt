9c1a5543650d967514eb5a6b13231fa380e1d311
rkdals213 <rkdals213@naver.com>
2022-09-10 13:31:54 +0900

    feat: 리팩터링

--- nok-backend/src/main/kotlin/com/example/nokbackend/security/JwtTokenProvider.kt
+++ nok-backend/src/main/kotlin/com/example/nokbackend/security/JwtTokenProvider.kt
@@ -12,13 +12,13 @@ const val TWELVE_HOURS_IN_MILLISECONDS: Long = 1000 * 60 * 60 * 12
 class JwtTokenProvider(
     private val signingKey: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256),
     private val expirationInMilliseconds: Long = TWELVE_HOURS_IN_MILLISECONDS,
-    private val CLAIM_EMAIL: String = "email"
+    private val claimEmail: String = "email"
 ) {
     fun createToken(payload: String): String {
         val now = Date()
         val expiration = Date(now.time + expirationInMilliseconds)
         return Jwts.builder()
-            .claim(CLAIM_EMAIL, payload)
+            .claim(claimEmail, payload)
             .setIssuedAt(now)
             .setExpiration(expiration)
             .signWith(signingKey)
@@ -30,7 +30,7 @@ class JwtTokenProvider(
             throw RuntimeException("로그인 정보가 정확하지 않습니다")
         }
 
-        return getClaimsJws(token).body[CLAIM_EMAIL]
+        return getClaimsJws(token).body[claimEmail]
             .toString()
     }
 
