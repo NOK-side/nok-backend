9ef3bda66424a5d3d99b2b3b7f1b5bf9843cd9c4
rkdals213 <rkdals213@naver.com>
2022-11-10 22:57:56 +0900

    feat: 운영디비 설정

--- nok-backend/src/main/kotlin/com/example/nokbackend/application/Constants.kt
+++ nok-backend/src/main/kotlin/com/example/nokbackend/application/Constants.kt
@@ -4,4 +4,4 @@ import java.math.BigDecimal
 
 val centerOfKoreaLongitude: BigDecimal = BigDecimal.valueOf(127.385712)
 val centerOfKoreaLatitude: BigDecimal = BigDecimal.valueOf(36.3501622)
-const val defaultDistance: Int = 1000 * 1000
+const val defaultDistance = 1000 * 1000
