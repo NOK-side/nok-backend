95383a5155b5f61edf07c64af1d4f24f0e521496
rkdals213 <rkdals213@naver.com>
2023-09-13 20:36:38 +0900

    fix : 리펙토링

--- nok-backend/src/main/kotlin/com/example/nokbackend/application/point/PointQueryService.kt
+++ nok-backend/src/main/kotlin/com/example/nokbackend/application/point/PointQueryService.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 class PointQueryService(
     private val memberPointRepository: MemberPointRepository,
     private val memberPointChargeRepository: MemberPointChargeRepository,
